3f52fd8b83b930ffdecebdd52f9589b113b41def
Beny Allan <benyallan@yahoo.com.br>
2025-03-03 15:38:47 -0300

    Updates the dockerfile structure, adds support to environmental variables and database configurations in the authentication service

--- auth-service/src/__init__.py
+++ auth-service/src/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
-from src.routes.auth import auth_bp
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Carrega as variáveis do .env
 
 def create_app():
     app = Flask(__name__)
 
-    # Registrar Blueprint de autenticação
-    app.register_blueprint(auth_bp, url_prefix="/auth")
+    # Configurações do banco de dados
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "default_secret_key")
 
     return app

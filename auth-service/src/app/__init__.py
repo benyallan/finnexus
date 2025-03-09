a815d3cccbbeb8d7551dc67f0aa81006b74267de
Beny Allan <benyallan@yahoo.com.br>
2025-03-09 12:06:05 -0300

    Atualiza o Dockerfile e configurações do ambiente: ajusta a URL do banco de dados, instala dependências do sistema e reorganiza a estrutura do projeto

--- auth-service/src/app/__init__.py
+++ auth-service/src/app/__init__.py
@@ -10,7 +10,7 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "default_secret_key")
 
-    from app.routes import init_routes
+    from .routes import init_routes
     init_routes(app)
 
     return app
--- auth-service/src/main.py
+++ auth-service/src/main.py
@@ -1,11 +1,7 @@
-from src import create_app
+from src.app import create_app
 from flask import Flask, jsonify
 
 app = create_app()
-
-@app.route('/hello', methods=['GET'])
-def hello():
-    return jsonify({"message": "Olá, mundo!"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

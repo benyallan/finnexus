c851d1c2cb11e6982fd1c2ca02550eec8affa3a7
Beny Allan <benyallan@yahoo.com.br>
2025-03-09 10:18:49 -0300

    Atualiza o Dockerfile para mudar o diretório de trabalho e copia todos os arquivos; adiciona rota de teste "hello" no serviço de autenticação

--- auth-service/src/main.py
+++ auth-service/src/main.py
@@ -1,6 +1,11 @@
 from __init__ import create_app
+from flask import jsonify
 
 app = create_app()
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return jsonify({"message": "Olá, mundo!"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
